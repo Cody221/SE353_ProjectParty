654b6a53c7da9e003b65778b090aa12f63fc08b0
Turbo1263 <45638439+Turbo1263@users.noreply.github.com>
2024-11-07 12:16:39 -0500

    add scripts

---
+++ UIScenes/CharacterSelect.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# Assign this variable to MPAuth node
+@export var auth: MPAuth
+
+func _ready():
+	auth.authenticate_function = _auth_check
+
+func _auth_check(plr_id: int, credentials_data: Dictionary, handshake_data: Dictionary):
+	# Return authentication data, otherwise if failed, return false.
+	return {}
